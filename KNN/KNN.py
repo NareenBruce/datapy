45381c4b0c38d35118b29023e87b51200eb4be57
Nareenthiran <nareenthiran5@gmail.com>
2025-03-22 22:25:45 +0800

    Update KNN.py

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -6,7 +6,6 @@ import numpy as np
 from sklearn import linear_model, preprocessing
 
 data= pd.read_csv("KNN\car.data")
-print(data.head())
 
 # Preprocessing the data/ making the data in integer format
 le = preprocessing.LabelEncoder()
@@ -17,7 +16,7 @@ persons = le.fit_transform(list(data["persons"]))
 lug_boot = le.fit_transform(list(data["lug_boot"]))
 safety = le.fit_transform(list(data["safety"]))
 cls = le.fit_transform(list(data["class"]))
-print(buying)
+
 
 predict = "class"
 
@@ -25,3 +24,17 @@ x= list(zip(buying, maint, doors, persons, lug_boot, safety))
 y= list(cls)
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
+model= KNeighborsClassifier(n_neighbors=5)
+
+model.fit(x_train, y_train)
+acc = model.score(x_test, y_test)
+print(acc)
+
+predicted = model.predict(x_test)
+names = ["unacc", "acc", "good", "vgood"]
+
+for x in range(len(predicted)):
+    print("Predicted: ", names[predicted[x]], "Data: ", x_test[x], "Actual: ", names[y_test[x]])
+    n = model.kneighbors([x_test[x]], 9, True)
+    print("N: \n", n)
