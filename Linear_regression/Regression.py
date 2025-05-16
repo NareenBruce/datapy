0225f43bd4abde58af44fb2757b31e8f81fbf4c8
Nareenthiran <nareenthiran5@gmail.com>
2025-05-16 11:28:32 +0800

    metrics

--- Linear_regression/Regression.py
+++ Linear_regression/Regression.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import pickle
@@ -14,34 +15,41 @@ predict = "G3"
 
 x = np.array(data.drop([predict], axis=1))
 y = np.array(data[predict])
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
+print(x.shape)
+print(y.shape)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=30)
 
-# best= 0
-# for _ in range(30):
-#     #splitting data into training and testing data
-#     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
+best= 0
+for _ in range(30):
 
-#     #create and train the model
-#     linear = linear_model.LinearRegression()
-#     linear.fit(x_train, y_train)
+    #create and train the model
+    linear = linear_model.LinearRegression()
+    linear.fit(x_train, y_train)
 
-#     # #testing or evaluate the model
-#     acc = linear.score(x_test, y_test) #models accuracy score
-#     print(acc)
+    # #testing or evaluate the model
+    acc = linear.score(x_test, y_test) 
 
-#     if acc > best:
-#         best = acc
-#         with open("studentmodel.pickle", "wb") as f:
-#             pickle.dump(linear, f)
+    if acc > best:
+        best = acc
+        with open("studentmodel.pickle", "wb") as f:
+            pickle.dump(linear, f)
     
 pickle_in = open("Linear_regression/studentmodel.pickle", "rb")
 linear = pickle.load(pickle_in)
-acc = linear.score(x_test, y_test) #models accuracy score
-print("Accuracy: ", acc)
+
+#Metrics
+r2= linear.score(x_test, y_test)
+predictions= linear.predict(x_test)
+mse= mean_squared_error(y_test, predictions)
+mae= mean_absolute_error(y_test, predictions)
+rmse= np.sqrt(mse)
+print("R2: ", r2)
+print("MAE: ", mae)
+print("RMSE: ", rmse)
 print("Coefficient: \n", linear.coef_)
 print("Intercept: \n", linear.intercept_)
 
-predictions= linear.predict(x_test)
+
 
 for x in range(len(predictions)):
     print("Mark prediction: ",predictions[x],"\n", x_test[x], "\n","Actual mark: ",y_test[x])
